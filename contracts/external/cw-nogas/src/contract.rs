60f6c025737bb3d2034a3056fed325bcdbdc620b
bekauz <bekauz@protonmail.com>
2023-02-12 22:50:41 +0100

    handling both compressed and uncompressed public keys for addr generation

--- contracts/external/cw-nogas/src/contract.rs
+++ contracts/external/cw-nogas/src/contract.rs
@@ -1,9 +1,10 @@
-use bech32::{Variant};
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Addr, StdError};
+use bech32::{Variant, ToBase32};
+use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult, Addr, StdError, to_binary};
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cw2::set_contract_version;
 use cw_storage_plus::Map;
+use ripemd::Ripemd160;
 use sha2::{Sha256, Digest};
 use crate::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, ExternalMessage};
@@ -32,108 +33,47 @@ pub fn execute(
     msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
     match msg {
-        ExecuteMsg::ProcessMessage { msg } => try_process_external_message(msg, deps, env, info),
+        ExecuteMsg::ProcessMessage { msg } => Ok(Response::default())
     }
 }
 
-fn try_process_external_message(
-    msg: ExternalMessage, 
-    deps: DepsMut, 
-    env: Env, 
-    info: MessageInfo
-) -> Result<Response, ContractError> {
-    // validate the nonce
-    let nonce = NONCES.load(deps.storage, msg.pk)?;
-    if msg.payload.nonce != nonce {
-        return Err(ContractError::InvalidNonce {  })
-    }
+pub const UNCOMPRESSED_PK_LENGTH: usize = 130;
+pub const COMPRESSED_PK_LENGTH: usize = 66;
 
-    // validate the payload signature
-    let mut sha = Sha256::new();
-    sha.update(msg.payload); // TODO
-    let payload_hash = sha.finalize();
+pub fn ec_pk_to_bech32_address(hex_pk: String, prefix: String) -> Result<Addr, ContractError> {
 
-    let verification = deps.api.secp256k1_verify(
-        payload_hash.as_slice(), 
-        &msg.signature, 
-        msg.pk.as_bytes(),
-    );
+    let raw_pk = match hex_pk.clone().len() {
+        // uncompressed pk: compress it and return the raw bytes
+        UNCOMPRESSED_PK_LENGTH => {
+            let raw_pk = hex_pk_to_raw_bytes(hex_pk.clone())?;
 
-    match verification {
-        Ok(valid_signature) => {
-            // error if payload had been tampered with
-            if !valid_signature {
-                return Err(ContractError::InvalidSignature {  })
+            // extract the compressed version of public key
+            let public_key = secp256k1::PublicKey::from_slice(
+                raw_pk.as_slice()
+            );
+
+            match public_key {
+                Ok(pk) => pk.serialize().to_vec(),
+                Err(e) => return Err(ContractError::Std(
+                    StdError::GenericErr { msg: e.to_string() },
+                )),
             }
         },
-        Err(e) => return Err(ContractError::Std(StdError::from(e))),
-    };
-
-    // if message has an expiration date, validate that
-    if let Some(timestamp) = msg.payload.expiration {
-        if timestamp.is_expired(&env.block) {
-            return Err(ContractError::ExpiredPayload {  })
-        }
-    }
-
-    // bump the nonce
-    NONCES.save(deps.storage, msg.pk, &(nonce + 1))?;
-
-    // relay the message on behalf of signer
-    info.sender = ec_pk_to_bech32_address(msg.pk, "juno".to_string())?;
-
-    // execute the inner message
-    let response = execute(
-        deps,
-        env,
-        info,
-        *msg.payload.msg,
-    )?;
-    
-    Ok(Response::new()
-        .add_attribute("outer_method", "try_process_external_message")
-        .add_attributes(response.attributes)
-    )
-}
-
-// takes an uncompressed EC public key and a prefix
-pub fn ec_pk_to_bech32_address(hex_pk: String, prefix: String) -> Result<Addr, ContractError> {
-    if hex_pk.clone().len() != 130 {
-        return Err(ContractError::Std(
-            StdError::InvalidHex {
-                msg: "unexpected hex encoded uncompressed public key length".to_string()
+        // compressed pk, return the raw bytes
+        COMPRESSED_PK_LENGTH => hex_pk_to_raw_bytes(hex_pk.clone())?,
+        _ => return Err(ContractError::Std(
+            StdError::GenericErr { 
+                msg: "Unexpected hex encoded public key length".to_string()
             }
-        ));
-    }
-
-    // get the raw public key bytes
-    let decoded_pk = hex::decode(hex_pk);
-    let raw_pk = match decoded_pk {
-        Ok(pk) => pk,
-        Err(e) => return Err(ContractError::Std(
-            StdError::InvalidHex { msg: e.to_string() })
-        ),
-    };
-
-    // extract the compressed version of public key
-    let public_key = secp256k1::PublicKey::from_slice(raw_pk.as_slice());
-    let raw_pk = match public_key {
-        Ok(pk) => pk.serialize().to_vec(),
-        Err(e) => return Err(ContractError::Std(
-            StdError::GenericErr { msg: e.to_string() },
         )),
     };
 
     // sha256 the raw public key
-    let mut hasher = Sha256::new();
-    hasher.update(raw_pk);
-    let pk_sha256 = hasher.finalize();
+    let pk_sha256 = Sha256::digest(raw_pk);
 
     // take the ripemd160 of the sha256 of the raw pk
-    let mut hasher = Ripemd160::new();
-    hasher.update(pk_sha256);
-    let address_raw = hasher.finalize();
-
+    let address_raw = Ripemd160::digest(pk_sha256);
+    
     // encode the prefix and the raw address bytes with Bech32
     let bech32 = bech32::encode(
         &prefix,
@@ -148,7 +88,17 @@ pub fn ec_pk_to_bech32_address(hex_pk: String, prefix: String) -> Result<Addr, C
         )),
     }
 }
- 
+
+fn hex_pk_to_raw_bytes(hex_pk: String) -> Result<Vec<u8>, ContractError> {
+    let decoded_pk = hex::decode(hex_pk);
+    match decoded_pk {
+        Ok(pk) => Ok(pk),
+        Err(e) => return Err(ContractError::Std(
+            StdError::InvalidHex { msg: e.to_string() })
+        ),
+    }
+}
+
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(_deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {}
--- contracts/external/cw-nogas/src/testing/tests.rs
+++ contracts/external/cw-nogas/src/testing/tests.rs
@@ -1,15 +1,71 @@
-use crate::contract::ec_pk_to_bech32_address;
+use cosmwasm_std::{StdError, Addr};
+
+use crate::{contract::ec_pk_to_bech32_address, ContractError};
+
+
+pub const JUNO_ADDRESS: &str = "juno1muw4rz9ml44wc6vssqrzkys4nuc3gylrxj4flw";
+pub const COMPRESSED_PK: &str = "03f620cd2e33d3f6af5a43d5b3ca3b9b7f653aa980ae56714cc5eb7637fd1eeb28";
+pub const UNCOMPRESSED_PK: &str = "04f620cd2e33d3f6af5a43d5b3ca3b9b7f653aa980ae56714cc5eb7637fd1eeb28fb722c0dacb5f005f583630dae8bbe7f5eaba70f129fc279d7ff421ae8c9eb79";
+pub const PREFIX: &str = "juno";
+
 
 #[test]
-fn test_generate_juno_addr_from_pk() -> Result<(), crate::ContractError>{
-
-    let juno_address = "juno1muw4rz9ml44wc6vssqrzkys4nuc3gylrxj4flw".to_string();
-    let juno_pk = "04f620cd2e33d3f6af5a43d5b3ca3b9b7f653aa980ae56714cc5eb7637fd1eeb28fb722c0dacb5f005f583630dae8bbe7f5eaba70f129fc279d7ff421ae8c9eb79".to_string();
+fn test_generate_addr_from_uncompressed_pk() -> Result<(), ContractError>{
 
     let generated_address = ec_pk_to_bech32_address(
-        juno_pk,
-        "juno".to_string()
+        UNCOMPRESSED_PK.to_string(),
+        PREFIX.to_string(),
     )?;
-    assert_eq!(generated_address, juno_address);
+    assert_eq!(generated_address, Addr::unchecked(JUNO_ADDRESS));
     Ok(())
 }
+
+#[test]
+fn test_generate_addr_from_compressed_pk() -> Result<(), ContractError> {
+
+    let generated_address = ec_pk_to_bech32_address(
+        COMPRESSED_PK.to_string(),
+        PREFIX.to_string(),
+    )?;
+    assert_eq!(generated_address, Addr::unchecked(JUNO_ADDRESS));
+    Ok(())
+}
+
+#[test]
+fn test_generate_addr_invalid_hex_length() {
+    let invalid_length_pk = "".to_string();
+
+    let err = ec_pk_to_bech32_address(
+        invalid_length_pk,
+    PREFIX.to_string()
+    )
+    .unwrap_err();
+
+    assert!(matches!(err, ContractError::Std(StdError::GenericErr { .. })));
+}
+
+#[test]
+fn test_generate_addr_not_hex_pk() {
+    let non_hex_pk = "03zzzzcd2e33d3f6af5a43d5b3ca3b9b7f653aa980ae56714cc5eb7637fd1eeb28".to_string();
+
+    let err = ec_pk_to_bech32_address(
+        non_hex_pk,
+    PREFIX.to_string()
+    )
+    .unwrap_err();
+
+    assert!(matches!(err, ContractError::Std(StdError::InvalidHex { .. })));
+}
+
+#[test]
+fn test_generate_addr_bech32_invalid_human_readable_part() {
+
+    let err = ec_pk_to_bech32_address(
+        UNCOMPRESSED_PK.to_string(),
+        "jUnO".to_string(),
+    )
+    .unwrap_err();
+
+    assert!(matches!(err, ContractError::Std(StdError::GenericErr { .. })));
+}
+
